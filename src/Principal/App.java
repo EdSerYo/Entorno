52daa76ca2716b81d1fa005f4803e06c0da23c24
alumno <alumno@DESKTOP-UE84JL7>
2022-11-14 20:20:53 +0100

    Hola Mundo

---
+++ src/Principal/App.java
@@ -0,0 +1,11 @@
+package Principal;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		System.out.println("Hola Mundo");
+	}
+
+}
